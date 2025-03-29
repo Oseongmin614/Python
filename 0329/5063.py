fb926a6c1723469a023a0800ef56fc1f1d41afd1
Oseongmin614 <snders@naver.com>
2025-03-29 17:49:18 +0900

    feat: 0329 algorithm

--- 0329/5063.py
+++ 0329/5063.py
@@ -1,6 +1,12 @@
 #r은 광고를 하지 않았을 때 수익
 # e는 광고를 했을 때의 수익,
 #c는 광고 비용
-
-
-
+i=int(input())
+for _ in range(i):
+    r,e,c = map(int,input().split())
+    if r==e-c:
+        print("does not matter")
+    elif r>e-c:
+        print("do not advertise")
+    else:
+        print("advertise")
