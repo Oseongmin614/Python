a8c7a1645f014e7e84b1e27e3bf90eb1795f7dd1
Oseongmin614 <snders@naver.com>
2025-04-19 17:49:10 +0900

    feat: 0419 algorithm

--- 0419/ballpass.py
+++ 0419/ballpass.py
@@ -1,8 +1,8 @@
 def solution(numbers, k):
-    j = 0    
+    index = 0
     for i in range(k-1):
-        j += 2
-        if len(numbers) <= j:
-            j-=len(numbers)
+        index += 2
+        if len(numbers) <= index:
+            index-=len(numbers)
 
-    return numbers[j]
+    return numbers[index]
