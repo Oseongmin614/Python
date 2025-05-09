cbdb7a3f0da7c47a17356fcb8ae839f1db1a2bc5
ohseongmin <snders@naver.com>
2025-05-09 22:09:58 +0900

    Feat:20250509

---
+++ 0509/5618.py
@@ -0,0 +1,18 @@
+from math import gcd, isqrt
+
+n = int(input())
+numbers = list(map(int, input().split()))
+
+if n == 2:
+    g = gcd(numbers[0], numbers[1])
+else:
+    g = gcd(numbers[0], gcd(numbers[1], numbers[2]))
+
+
+result = set()
+for i in range(1, isqrt(g) + 1):
+    if g % i == 0:
+        result.add(i)
+        result.add(g // i)
+for num in sorted(result):
+    print(num)
